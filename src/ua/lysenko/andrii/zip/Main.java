8ee549c9a57855f1aa45f1e04bab62d4ebf61471
andlys <senkoily@gmail.com>
2021-11-30 17:24:38 +0200

    add unzip functionality

--- src/ua/lysenko/andrii/zip/Main.java
+++ src/ua/lysenko/andrii/zip/Main.java
@@ -2,10 +2,13 @@ package ua.lysenko.andrii.zip;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String pathIn = "C:\\projects\\ZipCmd\\src\\ua\\lysenko\\andrii\\zip\\Zipper.java";
-//        String pathIn = "C:\\projects\\ZipCmd\\src";
-        String pathOut = "C:\\Users\\Andrii\\Desktop\\zip_file.zip";
-        Zipper.zip(pathIn, pathOut);
+    public static void main(String[] args) throws InterruptedException {
+//        String pathInput = "C:\\projects\\ZipCmd\\src\\ua\\lysenko\\andrii\\zip\\Zipper.java";
+        String pathInput = "C:\\projects\\ZipCmd\\src\\ua\\lysenko\\andrii\\zip";
+        String pathOfZipFile = "C:\\Users\\Andrii\\Desktop\\zip_file.zip";
+//        wait to open VisualVM manually in time to monitor memory consumption
+//        Thread.sleep(10000);
+        Zipper.zip(pathInput, pathOfZipFile);
+        UnZipper.unzip(pathOfZipFile);
     }
 }
